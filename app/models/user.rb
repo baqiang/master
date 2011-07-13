138f35a35737ccfc0dc7d84a3a877af048e21369
baqiang <baq.james@gmail.com>
2011-07-13 10:53:22 +0800

    micropost model created

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ActiveRecord::Base
   attr_accessor    :password
   attr_accessible :name, :email, :password, :password_confirmation
   
+  has_many(:microposts, :dependent => :destroy)
+  
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :name, :presence         => true,
@@ -67,4 +69,19 @@ class User < ActiveRecord::Base
 end
 
 
+
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer         not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#  salt               :string(255)
+#  admin              :boolean
+#
 
